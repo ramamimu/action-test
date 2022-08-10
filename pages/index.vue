744d90318fe8d7e31edf265481a3f25fbe4e55e0
ramamimu <ahhmaad.blogger@gmail.com>
2022-08-10 15:05:43 +0700

    feat: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <p class="text-3xl">hello world {{$store.state.counter}}</p>
+  <p>yang ini dicoba</p>
+  <p @click="$store.commit('increment')">tapi di statenya gk bsa response</p>  
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
